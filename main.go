78bd5eba6cbc7d2b3a251917211e519928a48509
Nitu Stefania-Elena <62988263+stefinitu@users.noreply.github.com>
2023-02-02 18:08:32 +0200

    Add files via upload

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"myproj/core"
 	"os"
+	"strconv"
 )
 
 func main() {
 	nodeType := os.Args[1]
 	switch nodeType {
 	case "master":
-		core.GetMasterNode().Start()
+		portValue := os.Args[2]
+		core.GetMasterNode(portValue).Start(portValue)
 	case "worker":
-		core.GetWorkerNode().Start()
+		portValue := os.Args[2]
+		core.GetWorkerNode(portValue).Start(portValue)
 	case "masterworker":
 		if os.Args[2] == "1" || os.Args[2] == "2" {
 			node2Type := os.Args[2]
-			core.GetMasterWorkerNode(node2Type).Start(node2Type)
+			port := os.Args[3]
+			nrDials := os.Args[4]
+			portDialArray := []string{}
+			nrDialsInt, _ := strconv.Atoi(nrDials)
+			for i := 0; i < nrDialsInt; i++ {
+				portDialArray = append(portDialArray, os.Args[5+i])
+			}
+			core.GetMasterWorkerNode(node2Type, port, nrDials, portDialArray).Start(node2Type, port, nrDials, portDialArray)
 		}
 	default:
 		panic("invalid node type")
