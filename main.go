8d10f4bfd492851ac3afcbbfb52a89cf9f55474b
Nitu Stefania-Elena <62988263+stefinitu@users.noreply.github.com>
2023-02-02 11:12:03 +0200

    After a lot of Hard Work

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
-	"core"
-	"fmt"
+	"myproj/core"
 	"os"
 )
 
 func main() {
 	nodeType := os.Args[1]
-	fmt.Println(nodeType)
 	switch nodeType {
 	case "master":
 		core.GetMasterNode().Start()
 	case "worker":
 		core.GetWorkerNode().Start()
+	case "masterworker":
+		if os.Args[2] == "1" || os.Args[2] == "2" {
+			node2Type := os.Args[2]
+			core.GetMasterWorkerNode(node2Type).Start(node2Type)
+		}
 	default:
 		panic("invalid node type")
 	}
+
 }
